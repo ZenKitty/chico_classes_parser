89bc967cd16ad79a395fcc4ff0e488b132274380
ZenKitty <duncanmuirh@gmail.com>
2022-02-08 15:37:23 -0800

    Moved single search to function, added help options

--- main_search.py
+++ main_search.py
@@ -1,74 +1,25 @@
 import json
+import textwrap
 import requests
 import argparse
 
-
-def main():
-    parser = argparse.ArgumentParser(description="Query current CSU Chico course schedule")
-
-    # Allow user to use Single class functions or multiclass functions
-    subparsers = parser.add_subparsers(title="Valid Subcommands", help="Type of query, S = Single, M = Multi", dest="command", required=True)
-    
-    solo_parser = subparsers.add_parser("S")
-    solo_parser.add_argument("name", nargs=1, type=str, help="Input class name. Format: CSCI-211")
-    solo_parser.add_argument("-l", "--lab", help="Include lab sections", action="store_true")
-    
-    multi_parser = subparsers.add_parser("M")
-    group2 = multi_parser.add_mutually_exclusive_group()
-    group2.add_argument("-b", "--best", help="Find the best time for an event, given a list of Subject Abbreviations", nargs="+", metavar="SUBJECT")
-    group2.add_argument("-w", "--worst", help="Find the best time for an event, given a list of Subject Abbreviations", nargs="+", metavar="SUBJECT")
-    
-    args = parser.parse_args()
-
-    # Figure out which command they're using
-    if args.command == "S":
-        subject, catalog_nbr = args.name[0].split('-')
-    elif args.command == "M":
-        print("Multi-Class implementation still in progress")
-        return
-    else:
-        print("Something went wrong")
-        return
-
-
-    # CSU Chico class schedule search
-    URL = "https://cmsweb.csuchico.edu/psc/CCHIPRD/EMPLOYEE/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch?"
-
+def single_class(subject, catalog_nbr, URL):
     PARAMS = {
         'institution':'CHICO',
         'term':'2202', # Term is a numerical representation of Spring/Summer/Fall/Winter term
-        'date_from':'',
-        'date_thru':'',
         'subject':subject, # 4 letter abbreviation, i.e. KINE = Kinesiology
-        'subject_like':'',
         'catalog_nbr':catalog_nbr, # Class number from catalog
-        'time_range':'',
-        'days':'',
-        'campus':'',
-        'location':'',
-        'x_acad_career':'',
-        'acad_group':'',
-        'rqmnt_designtn':'',
-        'instruction_mode':'',
-        'keyword':'',
-        'class_nbr':'',
-        'acad_org':'',
-        'enrl_stat':'',
-        'crse_attr':'',
-        'crse_attr_value':'',
-        'instructor_name':'',
-        'session_code':'',
-        'units':'',
-        'operationName':'',
-        'variables':'',
-        'query':''
     }
+
     class_list = requests.get(url=URL, params=PARAMS)
+    
     if class_list.status_code == 200:
         class_dict = class_list.json()
         print(class_list)
         print(f"Search Results for {subject}-{catalog_nbr}")
-        for counter, class_found in enumerate(class_dict):
+        if not bool(class_dict):
+            print("No classes found with that Subject and Catalog Number")
+        for class_found in class_dict:
             if class_found['component'] != "ACT" or args.lab:
                 print("Section {}:".format(class_found['class_section']))
                 for time in class_found['meetings']:
@@ -80,14 +31,56 @@ def main():
                     end_hour, end_minute, end_second, excess = time['end_time'].split('.')
                     end_hour_int = int(end_hour) if int(end_hour) <= 12 else int(end_hour) % 12
                     end_M = "PM" if int(end_hour) >= 12 else "AM"
-                    end_string = f"\t{end_hour_int}:{start_minute} {start_M}"
                     end_hour, end_minute, end_second, excess = time['end_time'].split('.')
+                    end_string = f"\t{end_hour_int}:{end_minute} {end_M}"
                     
                     print(start_string)
                     print(end_string)
     else:
         print(class_list.status_code)
         
+
+def main():
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent('''\
+        Query current CSU Chico course schedule
+
+        Subject Abbreviations and Course Numbers available at:
+        http://catalog.csuchico.edu/viewer/home
+        '''))
+    
+
+    # Allow user to use Single class functions or multiclass functions
+    subparsers = parser.add_subparsers(title="Valid Subcommands", help="Type of query, S = Single, M = Multi", dest="command", required=True)
+    
+    solo_parser = subparsers.add_parser("S")
+    solo_parser.add_argument("name", nargs=1, type=str, help="Input class name and number. Format: CSCI-211")
+    solo_parser.add_argument("-l", "--lab", help="Include lab sections", action="store_true")
+    solo_parser.add_help
+    
+    multi_parser = subparsers.add_parser("M")
+    group2 = multi_parser.add_mutually_exclusive_group()
+    group2.add_argument("-b", "--best", help="Find the best time for an event, given a list of Subject Abbreviations", nargs="+", metavar="SUBJECT")
+    group2.add_argument("-w", "--worst", help="Find the best time for an event, given a list of Subject Abbreviations", nargs="+", metavar="SUBJECT")
+    group2.add_argument("-time" "--time", help="Find how many classes start at a specific time. Format: 10:00 AM, or 2:00 PM", nargs=1, type=str, metavar="TIME")
+    
+    args = parser.parse_args()
+
+    # CSU Chico class schedule search
+    URL = "https://cmsweb.csuchico.edu/psc/CCHIPRD/EMPLOYEE/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch?"
+
+    # Figure out which command they're using
+    if args.command == "S":
+        subject, catalog_nbr = args.name[0].split('-')
+        single_class(subject, catalog_nbr, URL)
+    elif args.command == "M":
+        print("Multi-Class implementation still in progress")
+        return
+    else:
+        print("Something went wrong")
+        return
+
+
+
 
 if __name__ == "__main__":
     main()
